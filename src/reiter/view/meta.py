b052ea184d5d2e5441a661acda5640ad95d2d329
Souheil Chelfouh <trollfot@gmail.com>
2021-02-05 19:39:53 +0100

    Added handling of different kind of return

--- src/reiter/view/meta.py
+++ src/reiter/view/meta.py
@@ -1,12 +1,19 @@
-from typing import Callable, Generator, Tuple
+import inspect
+from typing import Callable, Generator, Tuple, Union, Dict
+from horseman.definitions import METHODS
 from horseman.http import HTTPError
+from horseman.meta import Overhead
+from horseman.response import Response
 from horseman.prototyping import WSGICallable, HTTPMethod
 
 
 Endpoint = Callable[[Overhead], WSGICallable]
+Result = Union[str, Dict, Tuple, Response]
 
 
 class View:
+
+    template = None
 
     def __init__(self, request: Overhead):
         self.request = request
@@ -15,10 +22,41 @@ class View:
     def update(self):
         pass
 
+    def namespace(self, **extra):
+        return {
+            'request': self.request,
+            'view': self,
+            **extra
+        }
+
+    def render(self, result: Result):
+        if isinstance(result, Response):
+            return result
+        if isinstance(result, str):
+            return Response.create(body=result)
+        if isinstance(result, tuple):
+            if len(result) == 1:
+                return Response.create(body=result[0])
+            if len(result) == 2:
+                body, code = result
+                return Response.create(body=body, code=code)
+            if len(result) == 3:
+                body, code, headers = result
+                return Response.create(body=body, code=code, headers=headers)
+            raise ValueError("Can't interpret return")
+        if isinstance(result, dict):
+            if self.template is None:
+                raise ValueError(
+                    "{self} returned a namespace but does "
+                    "not define a template")
+            ns = self.namespace(**result)
+            return self.request.app.ui.response(self.template, **ns)
+        return self
+
     def __call__(self):
         if worker := getattr(self, self.method, None):
             self.update()
-            return worker()
+            return self.render(worker())
         raise HTTPError(405)
 
     @classmethod
@@ -30,10 +68,18 @@ class View:
 def routables(view, methods=None) -> \
     Generator[Tuple[HTTPMethod, Endpoint], None, None]:
 
-    assert methods is None
-    assert inspect.isclass(view) and issubclass(view, GrokView), \
-        f'{view} needs to be a subclass of {GrokView}'
-
-    for method in METHODS:
-        if getattr(view, method, None) is not None:
-            yield method, view.resolve
+    if inspect.isfunction(view):
+        if methods is None:
+            methods = ['GET']
+        for method in methods:
+            if method not in METHODS:
+                raise ValueError(
+                    f"'{method}' is not a known HTTP method.")
+            yield method, view
+    else:
+        assert inspect.isclass(view) and issubclass(view, View), \
+            f'{view} needs to be a subclass of {View}'
+        assert methods is None
+        for method in METHODS:
+            if getattr(view, method, None) is not None:
+                yield method, view.resolve
